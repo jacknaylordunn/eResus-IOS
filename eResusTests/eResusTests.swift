6aca9164242d2eea4cc55664807a94d16e372e67
jacknaylordunn <jacknaylordunn@icloud.com>
2026-03-10 15:06:13 +0000

    1.1 bug fix - testing fix

---
+++ eResusTests/eResusTests.swift
@@ -0,0 +1,16 @@
+//
+//  eResusTests.swift
+//  eResusTests
+//
+//  Created by Jack Naylor Dunn on 07/03/2026.
+//
+
+import Testing
+
+struct eResusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
